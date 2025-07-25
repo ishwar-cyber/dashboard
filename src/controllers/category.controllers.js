2a7aaedab952a6dea919602df24f5323846fa9c5
ishwar-cyber <panditishwar21@gmail.com>
2025-07-25 16:58:56 +0530

    add category

--- src/controllers/category.controllers.js
+++ src/controllers/category.controllers.js
@@ -1,6 +1,6 @@
 import Category from "../modules/category.modules.js";
-import { createCategory } from "../services/category.service.js";
-import { uploadFile } from "../utilities/cloudnary.js";
+import { createCategory, getCategoryByIdOrSlug, updateCategoryById } from "../services/category.service.js";
+import { uploadFile, deleteFile } from "../utilities/cloudnary.js";
 export const create = async (req, res) => {
     try {
         const categoryData = {...req.body};
@@ -59,27 +59,29 @@ export const getCategoryById = async (req, res) => {
 
 export const updateCategory = async (req, res) => {
     try {
-        let category = await Category.findById(req.params.id);
-        if (!category) {
-            const error = new Error('Category not found');
-            error.statusCode = 404;
-            throw error;
-        }
-        const file = req.file; // Access the uploaded file from Multer
+        let category = {...req.body}
+        let id = req.params.id;
 
-        
-        const { name, status } = req.body;
-        category.name = name;
-        category.status = status;
-        await category.save();
+        const existingCategory = await getCategoryByIdOrSlug(id);
+        console.log('existingCategory', existingCategory);
+    
+
+        if(req.file && !existingCategory.image.public_id){
+            const result = await uploadFile(req.file.path);
+            category.image = {
+                url: result.url,
+                public_id: result.public_id
+            };
+        } else {
+            category.image = existingCategory.image;
+        }
+        const categoryUpdated = await updateCategoryById(id, category);
         res.status(200).json({
             success: true,
             message: 'Category updated successfully',
-            data: category
+            data: categoryUpdated
         });
     } catch (error) {
-      
-        
         res.status(error.statusCode || 500).json({
             success: false,
             message: error.message
--- src/services/category.service.js
+++ src/services/category.service.js
@@ -26,3 +26,35 @@ export const createCategory = async (categoryData) => {
         throw new Error(`Error creating category: ${error.message}`);
     }
 };
+
+export const getCategoryByIdOrSlug = async (idOrSlug) => {
+    try {
+        let category;
+        const query = idOrSlug.includes('-') ? { slug: idOrSlug } : { _id: idOrSlug };
+        if(/^[0-9a-fA-F]{24}$/.test(idOrSlug)){
+            category = await Category.findById(idOrSlug);
+        } else {
+            category = await Category.findOne(query);
+        }
+        if (!category) {
+            throw new Error('Category not found', 404);
+        }
+        return category;
+    } catch (error) {
+        throw new Error(`Error fetching category: ${error.message}`);
+    }
+};
+
+export const updateCategoryById = async (id, categoryData) => {
+    try {
+        const category = await Category.findById(id);
+        if (!category) {
+            throw new Error('Category not found', 404);
+        }
+        Object.assign(category, categoryData);
+        await category.save();
+        return category;
+    } catch (error) {
+        throw new Error(`Error updating category: ${error.message}`);
+    }
+};
