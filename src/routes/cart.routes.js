b0ee247908408263e58f5d4e756cdb38e8abfbc3
ishwar-cyber <61781204+ishwar-cyber@users.noreply.github.com>
2025-08-13 16:15:28 +0530

    Add get cart url

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import {addToCart, updateCartItem, removeItemFromCart} from '../controllers/cart.controllers.js';
+import {addToCart, getCart, updateCartItem, removeItemFromCart} from '../controllers/cart.controllers.js';
 import { tokenVerify } from '../middleware/auth.middlerwares.js';
 import { identifyVisitor, requireIdentification } from '../middleware/visitor.middleware.js';
 
 const cartRouter = Router();
 cartRouter.use(identifyVisitor);
 cartRouter.use(requireIdentification);
-
+cartRouter.get('/', getCart);
 cartRouter.post('/', addToCart);
 cartRouter.put('/:itemId', updateCartItem);
 cartRouter.delete('/:itemId', removeItemFromCart);
