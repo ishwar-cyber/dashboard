4ed2333f3201ec61b66e50665c4a17d801412b0e
ishwar-cyber <panditishwar21@gmail.com>
2025-08-18 23:21:02 +0530

    change visitor id generation

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import cartRouter from "./src/routes/cart.routes.js";
 import couponRouter from "./src/routes/coupon.routers.js";
 import subCategoryRouter from "./src/routes/sub_category.routes.js";
 import pincodeRouter from "./src/routes/service_pincode.routers.js";
-import { generateVisitorId } from './src/middleware/visitor.middleware.js';
+
 
 const app = express();
 
@@ -48,14 +48,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
-
-// ✅ Apply visitor middleware only for non-auth routes
-app.use((req, res, next) => {
-  if (!req.headers.authorization) {
-    return generateVisitorId(req, res, next);
-  }
-  next();
-});
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import {addToCart, getCart, clearCart, updateCartItem, removeItemFromCart} from '../controllers/cart.controllers.js';
-
+import { generateVisitorId } from '../middleware/visitor.middleware.js'
 const cartRouter = Router();
+// ✅ Apply visitor middleware only for non-auth routes
+cartRouter.use(generateVisitorId);
 cartRouter.get('/', getCart);
 cartRouter.put('/update/:id/quantity', updateCartItem);
 cartRouter.delete('/:id/remove', removeItemFromCart);
