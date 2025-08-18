66b3ca6556214900903671b85968e489ba3ce17a
ishwar-cyber <61781204+ishwar-cyber@users.noreply.github.com>
2025-08-18 15:43:36 +0530

    Update visitor.middleware.js

--- src/middleware/visitor.middleware.js
+++ src/middleware/visitor.middleware.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 export const generateVisitorId = (req, res, next) => {
   // If user already has visitorId cookie → use it
   let visitorId = req.cookies?.visitorId;
-  console.log('visitoredrttyy',req);
+  console.log('visitoredrttyy',req.cookies);
 
   if (!visitorId) {
     // ✅ Generate new one
