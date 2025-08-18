4c2c5daa74f7d5796f3d79a87b679303fda9b0d4
ishwar-cyber <61781204+ishwar-cyber@users.noreply.github.com>
2025-08-18 15:08:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import cartRouter from "./src/routes/cart.routes.js";
 import couponRouter from "./src/routes/coupon.routers.js";
 import subCategoryRouter from "./src/routes/sub_category.routes.js";
 import pincodeRouter from "./src/routes/service_pincode.routers.js";
-import { generateVisitorIds } from './src/middleware/visitor.middleware.js';
+import { generateVisitorId } from './src/middleware/visitor.middleware.js';
 
 const app = express();
 
