6a6d722913dae3dc74df0a6c68e512beea7212dd
ishwar-cyber <61781204+ishwar-cyber@users.noreply.github.com>
2025-08-18 10:49:50 +0530

    Update origin header

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization", "authToken"]
 };
 
 app.use(cors(corsOptions));
